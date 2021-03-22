02724f67f15b100daaed3717edde91a71b9452db
Орлов Ярослав <y.orlov@fonmix.ru>
2021-03-22 20:39:44 +0300

    'master: init commit Пн мар 22 20:39:44 MSK 2021'

--- hw1/functions.py
+++ hw1/functions.py
@@ -10,19 +10,42 @@ def to_involve(numbers, power=1):
     return list(map(pow, numbers, repeat(power)))
 
 
+def is_prime(number):
+    if number <= 1:
+        return False
+
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+
+    return True
+
+
 def filter_numbers(numbers, filter_type):
     if filter_type == EVEN:
-        return [filter(numbers)]
-
+        return list(filter(lambda x: x % 2 == 0, numbers))
+    if filter_type == ODD:
+        return list(filter(lambda x: x % 2 != 0, numbers))
+    if filter_type == SIMPLE_NUMBER:
+        return list(filter(is_prime, numbers))
 
 if __name__ == "__main__":
-    numbers = [4, 5, 2]
+    numbers = [0, 1, 4, 5, 2, 12, 7]
     power = 2
     print("Возвести список чисел = {numbers}, в степень = {power}".format(
         numbers=numbers, power=power
     ))
-    print("Результат = {retult}".format(
-        retult=to_involve(numbers, power=power)
-    ))
-    
-    filter_numbers(numbers, filter_type=EVEN)
+    power_retult = to_involve(numbers, power=power)
+    print("Результат = {retult}".format(retult=power_retult))
+
+    print("Вывести четные числа из списока = {numbers}".format(numbers=numbers))
+    even_numbers = filter_numbers(numbers, filter_type=EVEN)
+    print("Результат = {retult}".format(retult=even_numbers))
+
+    print("Вывести не четные числа из списока = {numbers}".format(numbers=numbers))
+    odd_numbers = filter_numbers(numbers, filter_type=ODD)
+    print("Результат = {retult}".format(retult=odd_numbers))
+
+    print("Вывести простые числа из списока = {numbers}".format(numbers=numbers))
+    simple_numbers = filter_numbers(numbers, filter_type=SIMPLE_NUMBER)
+    print("Результат = {retult}".format(retult=simple_numbers))
