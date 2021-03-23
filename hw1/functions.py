d59055e5ff1b6ffde24103dd3c73d0734b24a183
Орлов Ярослав <y.orlov@fonmix.ru>
2021-03-23 18:17:57 +0300

    'master: init commit Вт мар 23 18:17:57 MSK 2021'

--- hw1/functions.py
+++ hw1/functions.py
@@ -1,33 +1,36 @@
 
 from itertools import repeat
 
-EVEN = 'even'
-ODD = 'odd'
-SIMPLE_NUMBER = 'simple_number'
-
 
 def to_involve(numbers, power=1):
     return list(map(pow, numbers, repeat(power)))
 
 
-def is_prime(number):
-    if number <= 1:
-        return False
-
-    for i in range(2, number):
-        if number % i == 0:
-            return False
-
-    return True
-
-
 def filter_numbers(numbers, filter_type):
-    if filter_type == EVEN:
-        return list(filter(lambda x: x % 2 == 0, numbers))
-    if filter_type == ODD:
-        return list(filter(lambda x: x % 2 != 0, numbers))
-    if filter_type == SIMPLE_NUMBER:
+
+    def even(number_list):
+        return list(filter(lambda x: x % 2 == 0, number_list))
+
+    def odd(number_list):
+        return list(filter(lambda x: x % 2 != 0, number_list))
+
+    def is_prime(number):
+        if number <= 1:
+            return False
+        for i in range(2, number):
+            if number % i == 0:
+                return False
+        return True
+
+    def simple(number_list):
         return list(filter(is_prime, numbers))
+
+    filters = {
+        'even': even,
+        'odd': odd,
+        'simple': simple
+    }
+    return filters[filter_type](numbers)
 
 if __name__ == "__main__":
     numbers = [0, 1, 4, 5, 2, 12, 7]
@@ -39,13 +42,13 @@ if __name__ == "__main__":
     print("Результат = {retult}".format(retult=power_retult))
 
     print("Вывести четные числа из списока = {numbers}".format(numbers=numbers))
-    even_numbers = filter_numbers(numbers, filter_type=EVEN)
+    even_numbers = filter_numbers(numbers, filter_type='even')
     print("Результат = {retult}".format(retult=even_numbers))
 
     print("Вывести не четные числа из списока = {numbers}".format(numbers=numbers))
-    odd_numbers = filter_numbers(numbers, filter_type=ODD)
+    odd_numbers = filter_numbers(numbers, filter_type='odd')
     print("Результат = {retult}".format(retult=odd_numbers))
 
     print("Вывести простые числа из списока = {numbers}".format(numbers=numbers))
-    simple_numbers = filter_numbers(numbers, filter_type=SIMPLE_NUMBER)
+    simple_numbers = filter_numbers(numbers, filter_type='simple')
     print("Результат = {retult}".format(retult=simple_numbers))
